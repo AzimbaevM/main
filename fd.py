0a2b7efe31fdf21f5846cee48414f4f32fbc886b
AzimbaevM <azimbaevmuhammed98@gmail.com>
2025-08-04 18:40:10 +0600

    pp

--- fd.py
+++ fd.py
@@ -1,12 +1,27 @@
+#Домашняя работа:
 
-text = str(input('Введите текст: '))
-print(text[::-1])
+#Задание №1
+data = [10, 29, 30]
+data.append(47)
+data.append(89)
+data.insert(2,15)
+print(data)
 
+#Задание №2
+sa = [1, 5, 3, 5, 7, 5]
+sa.remove(5)
+print(sa)
 
-n = int(input('Введите сумму: '))
-if n >= 1000:
-    value = n / 10
-    value2 = n - value
-    print(f'Сумма: {value2}')
-if n < 1000:
-    print(f'Сумма: {n}')
+#Задание №3
+fruits = ['яблоко', 'банан', 'вишня', 'груша']
+i = fruits.index('груша')
+print(i)
+
+#Задание №4
+numbers = [8, 3, 5, 2, 9, 1]
+numbers.sort()
+print(numbers)
+
+#Задание №5
+num = [2, 4, 6, 8, 10, 12, 14]
+print(num[2::2])
